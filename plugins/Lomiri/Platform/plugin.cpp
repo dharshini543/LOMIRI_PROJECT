7b997e348be6c8a14a5ce203cd18552c66c5b2c3
Dharshini T R <dharshinitr1998@gmail.com>
2025-12-02 14:59:47 +0530

    plugins: Port LomiriPlatform plugin from Qt5 to Qt6

--- plugins/Lomiri/Platform/plugin.cpp
+++ plugins/Lomiri/Platform/plugin.cpp
@@ -22,6 +22,4 @@
 void GlobalShortcutPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QLatin1String("Lomiri.Platform"));
-
-    qmlRegisterSingletonType<Platform>(uri, 1, 0, "Platform", [](QQmlEngine*, QJSEngine*) -> QObject* { return new Platform; });
 }
